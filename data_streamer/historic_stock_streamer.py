a543a998eb477bab1921b212f86bf207561eece5
Yarden Moskovich <mosko169@gmail.com>
2017-01-24 22:32:04 +0200

    tiny bugfix in his sonifier

--- data_streamer/historic_stock_streamer.py
+++ data_streamer/historic_stock_streamer.py
@@ -37,7 +37,7 @@ class HistoricStockStreamer(SonifiableDataStreamer):
             'Volume': {SoundParams.pitch: PitchMapper(Consts.DEFAULT_TEMPO, Consts.DEFAULT_VOLUME, 1, self._volume_to_pitch)}}
 
     def _init_history(self, from_date, to_date):
-        self._historic_data = self._share.get_historical(from_date, to_date)
+        self._historic_data = list(reversed(self._share.get_historical(from_date, to_date)))
         unwanted_params = [unwanted_param for unwanted_param in self._historic_data[0].keys() if unwanted_param not in self._params]
 
         def format_day_data(unwanted_params, day):
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from manager import SonificationManager
 
 if __name__ == '__main__':
 #    streamer = SonifiableStockStreamer('GOOG')
-    historic_streamer = HistoricStockStreamer('ESLT', '2016-03-01', '2016-05-01', 10)
+    historic_streamer = HistoricStockStreamer('AAPL', '2016-01-01', '2016-04-01', 10)
     mapping = dict()
-    mapping['Close'] = (SoundParams.tempo, 114)
-    mapping['Volume'] = (SoundParams.pitch, 108)
+    mapping['Close'] = (SoundParams.tempo, 15)
+    mapping['Volume'] = (SoundParams.pitch, 14)
 
     manager = SonificationManager(historic_streamer, Sonifier(), mapping)
     manager.run()
